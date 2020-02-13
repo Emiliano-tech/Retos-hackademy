e49aeca87a02f5612b373377668a5b92205caf3a
Emiliano-tech <60978374+Emiliano-tech@users.noreply.github.com>
2020-02-13 16:04:53 -0600

    Add files via upload

---
+++ for-loop.js
@@ -0,0 +1,22 @@
+
+var limit = 10;
+var total = 0;
+for (let i = 1; i < limit; i++){
+    total +=  i;
+} 
+
+console.log(total);
+
+/* 
+    total = 1
+    total = 1 + 2 = 3
+    total 3 + 3 = 6
+    total 6 + 4 = 10 
+    total 10 + 5 = 15
+    total 15 +6 = 21
+    total 21 + 7 = 28
+    total 28 + 8 = 36
+    total 36 + 9 = 45
+    
+
+*/
---
+++ variables.js
@@ -0,0 +1,2 @@
+let example = "Some string";
+Console.log(example);
